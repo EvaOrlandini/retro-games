58ee40a129fda31f732f1fc336177c1f5ba7fa20
Eva Orlandini <evaorlandini.pro@gmail.com>
2025-02-17 12:05:17 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,12 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.initialize_on_precompile = false
+
+  # Activez la compression des assets
+  config.assets.compress = true
+  config.assets.js_compressor = nil
+  config.assets.css_compressor = nil
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
